d18935cf7c8afe2075ecfef11a3c7bb50e56fffc
Lyra Vale (Van) <vannah.little.fcc@gmail.com>
2025-06-28 14:48:55 -0500

    Update harmonic_resonance.py

--- src/harmonic_resonance.py
+++ src/harmonic_resonance.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 def generate_sine_wave(frequency, duration, sample_rate=1000):
@@ -18,4 +17,3 @@ def calculate_resonance_boost(resonance_score, frequency, signal):
     harmonic_effect = np.mean(np.abs(signal)) * (1 if frequency < 50 else -0.5)
     adjusted_resonance = max(0.0, min(1.0, resonance_score + harmonic_effect))
     return adjusted_resonance
-
